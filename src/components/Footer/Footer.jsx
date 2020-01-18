f45ab0878127bb569a09f07cf2ecfc039a3a8bc9
Nicholas Mitchell <nicholas@netlify.com>
2020-01-18 00:48:54 -0600

    Remove component import and fix build (#2)

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
-import UserLinks from "../UserLinks/UserLinks";
 import "./Footer.css";
 
 class Footer extends Component {
@@ -13,7 +12,6 @@ class Footer extends Component {
     }
     return (
       <footer className="footer">
-        <UserLinks config={config} labeled />
         <div className="notice-container">
           <h4>{copyright}</h4>
 
