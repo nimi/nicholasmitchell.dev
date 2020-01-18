e93001e1ee3ef30532cedd4234c3fc4e30b77457
Nicholas Mitchell <nicholas@netlify.com>
2020-01-18 00:30:30 -0600

    Remove public files and update gitignore

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -1,14 +1,14 @@
 const config = {
-  siteTitle: 'Gatsby Advanced Starter', // Site title.
-  siteTitleShort: 'GA Starter', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
-  siteTitleAlt: 'GatsbyJS Advanced Starter', // Alternative site title for SEO.
+  siteTitle: 'Nicholas Mitchell', // Site title.
+  siteTitleShort: 'NM Website', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
+  siteTitleAlt: 'Nicholas Mitchell', // Alternative site title for SEO.
   siteLogo: '/logos/logo-1024.png', // Logo used for SEO and manifest.
-  siteUrl: 'https://gatsby-advanced-starter-demo.netlify.com', // Domain of your website without pathPrefix.
+  siteUrl: 'https://asdf.netlify.com', // Domain of your website without pathPrefix.
   pathPrefix: '/', // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
-  siteDescription: 'A GatsbyJS stater with Advanced design in mind.', // Website description used for RSS feeds/meta description tag.
+  siteDescription: 'Website...', // Website description used for RSS feeds/meta description tag.
   siteRss: '/rss.xml', // Path to the RSS file.
-  siteFBAppID: '1825356251115265', // FB Application ID for using app insights
-  googleAnalyticsID: 'UA-47311644-5', // GA tracking ID.
+  siteFBAppID: '', // FB Application ID for using app insights
+  googleAnalyticsID: '', // GA tracking ID.
   postDefaultCategoryID: 'Tech', // Default category for posts.
   dateFromFormat: 'YYYY-MM-DD', // Date format used in the frontmatter.
   dateFormat: 'DD/MM/YYYY', // Date format for display.
@@ -22,23 +22,23 @@ const config = {
     "Yeah, I like animals better than people sometimes... Especially dogs. Dogs are the best. Every time you come home, they act like they haven't seen you in a year. And the good thing about dogs... is they got different dogs for different people.", // User description to display in the author segment.
   // Links to social profiles/projects you want to display in the author segment/navigation bar.
   userLinks: [
-    {
-      label: 'GitHub',
-      url: 'https://github.com/Vagr9K/gatsby-advanced-starter',
-      iconClassName: 'fa fa-github',
-    },
-    {
-      label: 'Twitter',
-      url: 'https://twitter.com/Vagr9K',
-      iconClassName: 'fa fa-twitter',
-    },
-    {
-      label: 'Email',
-      url: 'mailto:vagr9k@gmail.com',
-      iconClassName: 'fa fa-envelope',
-    },
+    // {
+    //   label: 'GitHub',
+    //   url: '',
+    //   iconClassName: 'fa fa-github',
+    // },
+    // {
+    //   label: 'Twitter',
+    //   url: '',
+    //   iconClassName: 'fa fa-twitter',
+    // },
+    // {
+    //   label: 'Email',
+    //   url: '',
+    //   iconClassName: 'fa fa-envelope',
+    // },
   ],
-  copyright: 'Copyright © 2019. Advanced User', // Copyright string for the footer of the website and RSS feed.
+  copyright: 'Copyright © 2020.', // Copyright string for the footer of the website and RSS feed.
   themeColor: '#c62828', // Used for setting manifest and progress theme colors.
   backgroundColor: '#e0e0e0', // Used for setting manifest background color.
 }
