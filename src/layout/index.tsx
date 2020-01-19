e088a0a8206ddc755ecfa2320d3c041022cf6297
Nicholas Mitchell <nicholas@netlify.com>
2020-01-19 01:11:48 -0600

    Add initial index page

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import Helmet from 'react-helmet'
 import styled from 'styled-components'
 
 import config from '../../data/SiteConfig'
+import Reset from '../components/Reset'
 
-function MainLayout(props: any) {
-  const { children } = props
+export interface MainLayoutProps {
+  children: ReactElement
+  padding: string
+}
+
+const FONT_HREF = 'https://fonts.googleapis.com/css?family=Lexend+Deca&display=swap'
+
+function MainLayout(props: MainLayoutProps) {
+  const { children, padding = '20px' } = props
   return (
-    <MainLayoutBox>
+    <MainLayoutBox padding={padding}>
+      <Reset />
       <Helmet>
         <meta name="description" content={config.siteDescription} />
         <html lang="en" />
+        <link href={FONT_HREF} rel="stylesheet"></link>
       </Helmet>
       {children}
     </MainLayoutBox>
@@ -21,29 +31,9 @@ export default MainLayout
 
 // styles
 
-const MainLayoutBox = styled.div`
+const MainLayoutBox = styled.div<{ padding: string }>`
   min-height: 100vh;
   min-width: 100vw;
   display: flex;
-  padding: 20px;
-
-  * {
-    box-sizing: border-box;
-  }
-
-  html {
-    height: 100%;
-  }
-
-  body {
-    margin: 0;
-    padding: 0;
-    min-height: 100%;
-    min-width: 100%;
-  }
-
-  #___gatsby #gatsby-focus-wrapper {
-    min-height: 100vh;
-    min-width: 100vw;
-  }
+  padding: ${({ padding }) => padding};
 `
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,75 @@
 import React from 'react'
 import Helmet from 'react-helmet'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
 import Layout from '../layout'
 import config from '../../data/SiteConfig'
+import GenerativeSketch from '../components/GenerativeSketch'
 
 function IndexPage() {
   return (
-    <Layout>
-      <div>
-        <Helmet title={`${config.siteTitle}`} />
-        index
-      </div>
+    <Layout padding="0px">
+      <Helmet title={`${config.siteTitle}`} />
+      <IndexBox>
+        <Flex>
+          <GenerativeSketch />
+        </Flex>
+        <Flex>
+          <IndexHeading>
+            Hi, I'm Nicholas. I'm a software engineer, listener, thinker, and problem solver.
+            Currently, I'm working with{' '}
+            <ExternalLink href="https://www.netlify.com">Netlify</ExternalLink> to help make apps
+            and websites faster, more secure and scalable.
+          </IndexHeading>
+          <IndexLinkList role="navigation">
+            <Link to="/posts">Work</Link>
+            <Link to="/posts">Blog</Link>
+            <Link to="/posts">Contact</Link>
+          </IndexLinkList>
+        </Flex>
+      </IndexBox>
     </Layout>
   )
 }
 
 export default IndexPage
+
+// styles
+
+const IndexBox = styled.div`
+  display: flex;
+  height: 100vh;
+  width: 100%;
+`
+
+const Flex = styled.div`
+  flex: 0 0 50%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const IndexHeading = styled.h2`
+  font-size: 3vw;
+  line-height: 3.5vw;
+  padding: 2vw 2vw 0 2vw;
+`
+
+const IndexLinkList = styled.div`
+  display: flex;
+  padding: 2vw;
+
+  height: 100%;
+  width: 100%;
+
+  > a {
+    align-self: flex-end;
+    font-size: 2vw;
+    line-height: 2.5vw;
+    flex: 1;
+  }
+`
+
+const ExternalLink = styled.a``
